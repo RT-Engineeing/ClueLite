036851cb8d08abee10263a3f909b4ee3911a8cd6
Samuel Eliot Nathanson <sen52@drexel.edu>
2021-03-02 20:43:07 -0500

    Committing React UI base to Repo

---
+++ UI/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Board from './Board'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="board">
+        <Board />
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
